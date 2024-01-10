7376ec835e859d716d9d3dcbc923698bdb6f50aa
Nabeel Yaseen <nabeelyaseenggwp@gmail.com>
2024-01-10 22:49:27 +0500

    1.8

--- resourses/js/javascript.js
+++ resourses/js/javascript.js
@@ -1,1 +1,20 @@
-const myset = new Set([1,2,34,5,6,7]);
+function* generator(i){
+    let x = 0;
+    while(true){
+        if(i){
+            x+=i;
+        }
+        yield x;
+        x++
+    }
+}
+
+const gen = generator(0);
+
+console.log(gen.next().value);
+console.log(gen.next(3).value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value); 
+console.log(gen.next().value);
