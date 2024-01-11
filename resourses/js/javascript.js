bba62a05552d4b4229d03c1bf57adaabfccfc392
Nabeel Yaseen <nabeelyaseenggwp@gmail.com>
2024-01-11 22:21:06 +0500

    1.9

--- resourses/js/javascript.js
+++ resourses/js/javascript.js
@@ -1,20 +1,11 @@
-function* generator(i){
-    let x = 0;
-    while(true){
-        if(i){
-            x+=i;
-        }
-        yield x;
-        x++
-    }
+"use strict";
+
+
+function midpoint(){
+    return (
+        (Math.min.apply(null ,arguments) + Math.max.apply(null ,arguments)) / 2
+    );
 }
 
-const gen = generator(0);
 
-console.log(gen.next().value);
-console.log(gen.next(3).value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value); 
-console.log(gen.next().value);
+console.log(midpoint(1,23,4,5,6));
