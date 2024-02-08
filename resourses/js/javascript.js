1e0b757500f21477c0fa933f376bfc1f7ba3cbbe
Nabeel Yaseen <40717758+develop-webkit@users.noreply.github.com>
2024-02-08 22:01:01 +0500

    1.5

--- resourses/js/javascript.js
+++ resourses/js/javascript.js
@@ -11,19 +11,19 @@ const inventor = [
 
 const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', ' Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', ' Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', ' Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', ' Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi' , 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken' , 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William']; 
 
-// const vintageInv = inventor.filter(cur => cur.year >= 1500 && cur.year < 1599 );
-// console.log(vintageInv)
+const vintageInv = inventor.filter(cur => cur.year >= 1500 && cur.year < 1599 );
+console.log(vintageInv)  
 
-// const invFullName = inventor.map(cur => `${cur.first} ${cur.last}`);
-// console.log(invFullName)
+const invFullName = inventor.map(cur => `${cur.first} ${cur.last}`);
+console.log(invFullName)
 
-// const sortInv = inventor.sort( (a,b) => (a.year - b.year) );
-// console.log(sortInv);
+const sortInv = inventor.sort( (a,b) => (a.year - b.year) );
+console.log(sortInv);
 
-// const invLife = inventor.reduce(reduceFunc,0);
-// function reduceFunc(total,cur){
-//     return total += (cur.passed - cur.year)
-// }
+const invLife = inventor.reduce(reduceFunc,0);
+function reduceFunc(total,cur){
+    return total += (cur.passed - cur.year)
+}
 
 const inventorAge = inventor.sort((a,b)=> { console.log((b.passed - b.year), (a.passed - a.year)); return (b.passed - b.year) - (a.passed - a.year) });
 console.table(inventorAge);
